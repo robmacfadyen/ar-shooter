a40ee92e6d92b051bbd62634d49449c8f5631241
Rob Macfadyen <robert.macfadyen@students.plymouth.ac.uk>
2017-10-30 00:48:39 +0000

    Kudan demo project

---
+++ KudanDemo/Assets/Scripts/Shoot.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour
+{
+    private float bullets;
+    [SerializeField]
+    private float maxBullets = 20;
+    [SerializeField]
+    private float fireTime = 0.5f;
+    [SerializeField]
+    private float reloadTime = 1.5f;
+    private bool canShoot = true;
+
+    [SerializeField]
+    private Color normalColor;
+    [SerializeField]
+    private Color shootColor;
+
+
+    [SerializeField]
+    private Image crosshair;
+
+    // Use this for initialization
+    public void Trigger()
+    {
+        if (canShoot)
+        {
+            crosshair.color = shootColor;
+            canShoot = false;
+            Invoke("SetCanShoot", fireTime);
+
+            ShootRay();
+        }
+    }
+
+    private void SetCanShoot()
+    {
+        crosshair.color = normalColor;
+        canShoot = true;
+    }
+
+    private void ShootRay()
+    {
+        // cast a ray from the crosshair
+
+        // return a list of targets hit
+
+        // call Shot method on the first target
+    }
+
+    public void Reload()
+    {
+        bullets = maxBullets;
+        crosshair.color = shootColor;
+        canShoot = false;
+        Invoke("SetCanShoot", reloadTime);
+    }
+}
