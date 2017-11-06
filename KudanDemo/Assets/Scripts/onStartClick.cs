4e85d3ccea4bb41479771e11f8d359d9c9073721
tylerhaskins <TylerHaskins@bitbucket.org>
2017-11-06 21:40:00 +0000

    Added script for the start button click to load up the main level

---
+++ KudanDemo/Assets/Scripts/onStartClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onStartClick : MonoBehaviour {
+
+    public delegate void Click();
+    public string levelName = "";
+	public void onClick()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
