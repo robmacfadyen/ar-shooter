612dbc4a9712ac875c783aa673abce347bd1a606
(s) Robert Macfadyen <rjmacfadyen>
2017-12-13 12:05:24 +0000

    Added Mapbox plugin

---
+++ KudanDemo/Assets/Scripts/GPS.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GPS : MonoBehaviour
+{
+    [SerializeField]
+    private Text debugText;
+
+    IEnumerator Start()
+    {
+
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("not enabled");
+            yield break;
+        }
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+            Debug.Log(maxWait);
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            debugText.text = ("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            debugText.text = ("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            debugText.text = ("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+    }
+}
---
+++ KudanDemo/Assets/Scripts/RasterMap.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using Mapbox.Map;
+using Mapbox.Unity;
+using Mapbox.Utils;
+using System;
+
+public class RasterMap : MonoBehaviour, Mapbox.Utils.IObserver<RasterTile>
+{
+    public float zoom = 5f;
+    public float tileCoordinateX = 0f;
+    public float tileCoordinateY = 51f;
+
+    void Start()
+    {
+        var map = new Map<RasterTile>(MapboxAccess.Instance);
+        map.Zoom = 2;
+        map.Vector2dBounds = Vector2dBounds.World();
+        map.MapId = "mapbox://styles/mapbox/satellite-streets-v10";
+        map.Subscribe(this);
+        map.Update();
+    }
+
+    public void OnNext(RasterTile tile)
+    {
+        if (tile.CurrentState == Tile.State.Loaded)
+        {
+            if (tile.HasError)
+            {
+                Debug.Log("RasterMap: " + tile.ExceptionsAsString);
+                return;
+            }
+
+            var tileQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            tileQuad.transform.SetParent(transform);
+            tileQuad.name = tile.Id.ToString();
+            tileQuad.transform.position = new Vector3(tile.Id.X, -tile.Id.Y, 0);
+            var texture = new Texture2D(0, 0);
+            texture.LoadImage(tile.Data);
+            var material = new Material(Shader.Find("Unlit/Texture"));
+            material.mainTexture = texture;
+            tileQuad.GetComponent<MeshRenderer>().sharedMaterial = material;
+        }
+    }
+
+    //public void Update()
+    //{
+    //    var parameters = new Tile.Parameters();
+    //    parameters.Fs = MapboxAccess.Instance;
+    //    parameters.Id = new CanonicalTileId((int)zoom, (int)tileCoordinateX, (int)tileCoordinateY);
+    //    parameters.MapId = "mapbox://styles/mapbox/satellite-v9";
+    //    var rasterTile = new RasterTile();
+
+    //    // Make the request.
+    //    rasterTile.Initialize(parameters, (Action)(() =>
+    //    {
+    //        if (!string.IsNullOrEmpty(rasterTile.Error))
+    //        {
+    //            // Handle the error.
+    //        }
+
+    //        // Consume the Data.
+    //    }));
+
+    //}
+}
